4940a99e9b4657cf1bc194f68774b6e96c9737f1
Rachel <c00284406@itcarlow.ie>
2024-02-26 19:48:14 +0000

    lab5

---
+++ Practicals/lab5/README.md
@@ -0,0 +1,43 @@
+# Project Title: Autoparts Direct
+
+## Table of Contents
+- [Introduction](#introduction)
+- [Target Audience](#target-audience)
+- [Key Features](#key-features)
+- [Learning Reflections](#learning-reflections)
+- [Development Updates](#development-updates)
+
+## Introduction
+This is a car parts website named "Autoparts". Here you can find unusual, yet cute, car parts at affordable prices.
+
+## Target Audience
+This website is aimed towards car lovers who want to spice up their car's appearance without breaking the bank.
+
+## Key Features
+- **Lab 1:** Web pages with a header, "about us" paragraph, and images of featured products with a search bar
+- **Lab 2:** Contact form with appropriate error checking
+- **Lab 3:** Flexbox and grids instead of tables
+- **Lab 4:** Clickable car - changes car and horn sound
+- **Lab 5:** Site is much more responsive now - grid reduces columns when necessary to fit page
+
+## Learning Reflections
+### Challenges
+- **Lab 1:** Understanding HTML tables, integrating images and centering the images was challenging but rewarding.
+- **Lab 2:** Using javascript to check the form and lead off to different pages took some time to get used to.
+- **Lab 3:** Learning how to utilise and style flexboxes and grids over tables
+- **Lab 4:** Learning how to check if source has particular string inside
+- **Lab 5:** Libraries won't work - they load in (confirmed in console) but won't run any functions
+
+### Insights
+- **Lab 1:** Learned the importance of clear and organised HTML for website structure.
+- **Lab 2:** Discovered how javascript can enhance the overall experience of a website
+- **Lab 3:** Discovered how much better flexboxes and grids are than tables
+- **Lab 4:** Discovered an easier way to check for string in large bodies of text
+- **Lab 5:** Responsive sites have a nicer feel to them
+
+## Development Updates
+- **Lab 1:** Launched a basic webpage with product images and descriptions.
+- **Lab 2:** Updated the website with a contact form and new pages for sale, new in, etc.
+- **Lab 3:** Replaced table on "New" and "Sale" with flexbox, replaced main table on home page to grid.
+- **Lab 4:** Clickable car on home screen that changes the car and plays a horn sound
+- **Lab 5:** Replaced all flexboxes with grid, made site more responsive, fixed car horns
---
+++ Practicals/lab5/assets/js/script.js
@@ -0,0 +1,135 @@
+  // Get popup by id
+  var popup = document.getElementById("salePopup");
+
+// Alert of sale on load.
+let homeBody = document.getElementById("home");
+homeBody.onload = function()
+{
+  // open popup
+  popup.style.display = "block";
+}
+
+
+
+
+function searchFunction()
+{
+    // Declare variables
+    var input;  // search bar
+    var filter; // change text to one case
+    var table;  // table values
+    var tr;     // table rows
+    var td;     // table data
+    var i;      // search array
+    var text;   // text inputted
+
+    input = document.getElementById("searchBar"); // assign html bar to input
+
+    filter = input.value.toUpperCase(); // change value to uppercase - easier to filter through
+
+    table = document.getElementById("itemTable"); // get table by id
+
+    tr = table.getElementsByTagName("tr"); // get table rows by id - find data
+  
+    // LOOP TO FIND LETTER/WORD INPUTTED
+    for (i = 0; i < tr.length; i++) 
+    {
+      td = tr[i].getElementsByTagName("td")[0]; // get each table data cell per row
+
+      if (td) 
+      {
+        text = td.textContent || td.innerText; // if text matches td text
+
+        if (text.toUpperCase().indexOf(filter) > -1)
+        {
+          tr[i].style.display = ""; // if match, make show 
+        } 
+        else 
+        {
+          tr[i].style.display = "none"; // if no match, make disappear
+        }
+      }
+    }
+}
+
+// Get the element that closes popup
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks x, close popup
+span.onclick = function() {
+  popup.style.display = "none";
+}
+
+// When the user clicks outside of popup, close it
+homeBody.onclick = function(event) {
+  if (event.target == popup) {
+    popup.style.display = "none";
+  }
+}
+
+
+function checkForm(){
+  if(document.getElementById("nameBox").value === ""){
+    alert("Please fill out your name.");
+  }
+  else if(document.getElementById("emailBox").value === ""){
+    alert("Please fill out your email.");
+  }
+  else if(document.getElementById("detailBox").innerHTML === ""){
+    alert("Please fill out the details box.");
+  }
+  else
+  {
+    window.open("success.html");
+  }
+}
+
+function changeCar(){
+  if(document.getElementById("car").src.indexOf("assets/img/car.png") !== -1){
+
+    document.getElementById("car").src = "assets/img/truck.png";
+
+    let truckHorn = new Audio();
+    truckHorn.src = "assets/audio/truckHorn.mp3";
+
+    truckHorn.play();
+
+  }
+  else if(document.getElementById("car").src.indexOf("assets/img/truck.png") !== -1){
+
+    document.getElementById("car").src = "assets/img/classic.png";
+
+    let classicHorn = new Audio();
+    classicHorn.src = "assets/audio/classicHorn.wav";
+  
+    classicHorn.play();
+  }
+  else if(document.getElementById("car").src.indexOf("assets/img/classic.png") !== -1){
+    
+    document.getElementById("car").src = "assets/img/miata.png";
+
+    let miataHorn = new Audio();
+    miataHorn.src = "assets/audio/miataHorn.wav";
+
+    miataHorn.play();
+  } 
+  else if(document.getElementById("car").src.indexOf("assets/img/miata.png") !== -1){
+    
+    document.getElementById("car").src = "assets/img/car.png";
+
+    let carHorn = new Audio();
+    carHorn.src = "assets/audio/carHorn.mp3";
+
+    carHorn.play();
+  }
+}
+
+function hamburger(){
+  var x = document.getElementById("myNav");
+  if (x.className === "nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav";
+  }
+}
+
