e9a5411b6473ae76b803ec464a6f58c732735db6
Rachel <c00284406@itcarlow.ie>
2024-02-29 12:52:42 +0000

    removed lab4 search

--- Practicals/lab4/assets/js/script.js
+++ Practicals/lab4/assets/js/script.js
@@ -6,48 +6,6 @@ homeBody.onload = function()
   popup.style.display = "block";
 }
 
-function searchFunction()
-{
-    // Declare variables
-    var input;  // search bar
-    var filter; // change text to one case
-    var table;  // table values
-    var tr;     // table rows
-    var td;     // table data
-    var i;      // search array
-    var text;   // text inputted
-
-    input = document.getElementById("searchBar"); // assign html bar to input
-
-    filter = input.value.toUpperCase(); // change value to uppercase - easier to filter through
-
-    table = document.getElementById("itemTable"); // get table by id
-
-    tr = table.getElementsByTagName("tr"); // get table rows by id - find data
-  
-    // LOOP TO FIND LETTER/WORD INPUTTED
-    for (i = 0; i < tr.length; i++) 
-    {
-      td = tr[i].getElementsByTagName("td")[0]; // get each table data cell per row
-
-      if (td) 
-      {
-        text = td.textContent || td.innerText; // if text matches td text
-
-        if (text.toUpperCase().indexOf(filter) > -1)
-        {
-          tr[i].style.display = ""; // if match, make show 
-        } 
-        else 
-        {
-          tr[i].style.display = "none"; // if no match, make disappear
-        }
-      }
-    }
-}
-
-
-
 // Get popup by id
 var popup = document.getElementById("salePopup");
 
